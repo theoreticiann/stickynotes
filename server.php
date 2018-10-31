557b506881f557c33c4a2133b95135874316a370
theoretician <38468093+theoreticiann@users.noreply.github.com>
2018-10-31 18:32:49 +0000

    Update server.php

--- server.php
+++ server.php
@@ -71,7 +71,7 @@ if (isset($_POST['login_user'])) {
   }
 
   if (count($errors) == 0) {
-  	$password = md5($hash);
+  	$hash = md5($password);
   	$query = "SELECT * FROM admin WHERE username='$username' AND password='$hash'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
