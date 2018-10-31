fe07523448e1ad1d1ff6b2ab29227a815038620b
theoretician <38468093+theoreticiann@users.noreply.github.com>
2018-10-31 09:48:56 +0000

    Update server.php

--- server.php
+++ server.php
@@ -72,7 +72,7 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+  	$query = "SELECT * FROM admin WHERE username='$username' AND password='$hash'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
