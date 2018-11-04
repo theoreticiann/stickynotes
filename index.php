6039534b900b0cfc8c5e60ac10e8ac80f3630454
Joseph Sunny <josephsunny@Josephs-MacBook-Air.local>
2018-11-04 00:25:59 +0000

    removed api

--- index.php
+++ index.php
@@ -39,27 +39,6 @@
 ?>
 
 
-<?php
-	$apiKey = "ade039452c8d195dee785fc32ad2cd54";
-	$cityId = "1270260";
-	$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
-
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_VERBOSE, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$response = curl_exec($ch);
-
-	curl_close($ch);
-	$data = json_decode($response);
-	$currentTime = time();
-?>
-
-
 
 <!DOCTYPE html>
 <html>
